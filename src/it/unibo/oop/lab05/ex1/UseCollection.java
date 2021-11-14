b10c690014480dcad7ffec0e34a7e6f7176fc926
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-11-14 17:36:12 +0100

    done ex1

--- src/it/unibo/oop/lab05/ex1/UseCollection.java
+++ src/it/unibo/oop/lab05/ex1/UseCollection.java
@@ -11,36 +11,36 @@ import java.util.Collection;
  */
 public final class UseCollection {
 
-    private UseCollection() {
-    }
+	private UseCollection() {
+	}
 
-    /**
-     * @param args
-     *            unused
-     */
-    public static void main(final String[] args) {
-
-        /*
-         * Unless you need to perform specific operations, always use the most
-         * generic interface.
-         * If we use `var` instead of specifying Collection<Integer> what type
-         * will `coll` have?
-         */
-        final Collection<Integer> coll = new ArrayList<>();
-        coll.addAll(Arrays.asList(1, 3, 5, 7, 9, 11)); // var-args
-        System.out.println(coll); // [1, 3, 5, 7, 9, 11]
-
-        coll.add(13);
-        coll.add(15);
-        coll.add(15);
-        coll.remove(7);
-        System.out.println(coll);
-
-        coll.removeAll(Arrays.asList(11, 13, 15));
-        System.out.println(coll);
-        coll.retainAll(Arrays.asList(1, 2, 3, 4, 5));
-        System.out.println(coll);
-        System.out.println(coll.contains(3));
-        System.out.println(Arrays.toString(coll.toArray()));
-    }
+	/**
+	 * @param args
+	 *            unused
+	 */
+	public static void main(final String[] args) {
+	
+		/*
+		 * Unless you need to perform specific operations, always use the most
+		 * generic interface.
+		 * If we use `var` instead of specifying Collection<Integer> what type
+		 * will `coll` have?  It will have type 'Object'
+		 */
+		final Collection<Integer> coll = new ArrayList<>();
+		coll.addAll(Arrays.asList(1, 3, 5, 7, 9, 11)); // var-args
+		System.out.println(coll); // [1, 3, 5, 7, 9, 11]
+	
+		coll.add(13);
+		coll.add(15);
+		coll.add(15);
+		coll.remove(7);
+		System.out.println(coll);
+	
+		coll.removeAll(Arrays.asList(11, 13, 15));
+		System.out.println(coll);
+		coll.retainAll(Arrays.asList(1, 2, 3, 4, 5));
+		System.out.println(coll);
+		System.out.println(coll.contains(3));
+		System.out.println(Arrays.toString(coll.toArray()));
+	}
 }
--- src/it/unibo/oop/lab05/ex1/UseSet.java
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -1,6 +1,9 @@
 package it.unibo.oop.lab05.ex1;
 
+import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
+//import java.util.function.Predicate;
 
 /**
  * Example class using {@link Set}.
@@ -8,31 +11,91 @@ import java.util.Set;
  */
 public final class UseSet {
 
-    private UseSet() {
-    }
+	static final boolean SET_RANDOMGEN = false;
 
-    /**
-     * @param args
-     *            ignored
-     */
-    public static void main(final String[] args) {
-        /*
-         * Considering the content of "UseCollection, write a program which, in
-         * order:
-         * 
-         * 1) Builds a TreeSet containing Strings
-         * 
-         * 2) Populates such Collection with all the Strings ranging from "1" to
-         * "20"
-         * 
-         * 3) Prints its content
-         * 
-         * 4) Removes all those strings whose represented number is divisible by
-         * three
-         * 
-         * 5) Prints the content of the Set using a for-each costruct
-         * 
-         * 6) Verifies if all the numbers left in the set are even
-         */
-    }
+	private UseSet() {
+	}
+
+	/**
+	 * @param args
+	 *            ignored
+	 */
+	public static void main(final String[] args) {
+	
+		// 1 - Builds a TreeSet containing Strings
+		final TreeSet<String> tree = new TreeSet<>();
+	
+		/*
+		 * 2 - Populates such Collection with all the Strings ranging from
+		 *   "1" to "20"
+		 */
+		for(int i = 1; i <= 20; i++) {
+			if(UseSet.SET_RANDOMGEN) {
+				tree.add(Integer.toString((int) (i * 100 * Math.random())));
+			} else {
+				tree.add(Integer.toString(i));
+			}
+		}
+	
+		// 3 - Prints its content
+		System.out.println(tree.toString());
+
+		/*
+		 * 4 - Removes all those strings whose represented number is divisible by
+		 * three
+		 */
+		{
+			Iterator<String> iterator = tree.iterator();
+			
+			while(iterator.hasNext()) {
+				String chk = iterator.next();
+				if(Integer.parseInt(chk) % 3 == 0) {
+					//tree.remove(chk);
+					iterator.remove();
+				}
+			}
+		} 
+		/*
+		 * 4 - Removes all those strings whose represented number is divisible by
+		 * three
+		 */
+		/*
+			Iterator<String> iterator = tree.iterator();
+			while(iterator.hasNext()) {
+				String chk = iterator.next();
+				
+				int sumValue = 0;
+				for(int index = 0; index < chk.length(); index++) {
+					sumValue += Integer.parseInt(Character.toString(chk.charAt(index)));
+				}
+				//System.out.print(sumValue);
+				if(sumValue % 3 == 0) iterator.remove();
+			}
+		}*/
+	
+		// 5 - Prints the content of the Set
+		System.out.println(tree.toString());
+	
+		// 5 - Prints the content of the Set using a for-each construct
+		Iterator<String> iterator = tree.iterator();
+		System.out.print("[");
+		while(iterator.hasNext()) {
+			String chk = iterator.next();
+			System.out.print(chk + ", ");
+		}
+		System.out.println("]");
+	
+		// 6 - Verifies if all the numbers left in the set are even
+		boolean areAllEven = true;
+		while(iterator.hasNext()) {
+			String chk = iterator.next();
+			areAllEven = areAllEven && (Integer.parseInt(chk) % 2 == 0);
+			if(Integer.parseInt(chk) % 3 == 0);
+		}
+		if(areAllEven) {
+			System.out.println("Element in the set are all even.");
+		} else {
+			System.out.println("Some element in the set are odd!");
+		}
+	}
 }
