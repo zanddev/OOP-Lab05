48a52099be57ab8bd09e9cce0fddd5b4afd8a613
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-12-09 17:18:16 +0100

    done class ex2.UseSetWithOrder

--- src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
+++ src/it/unibo/oop/lab05/ex2/UseSetWithOrder.java
@@ -1,28 +1,37 @@
 package it.unibo.oop.lab05.ex2;
 
+import java.util.TreeSet;
+
 /**
  * 
  */
 public final class UseSetWithOrder {
 
-    private UseSetWithOrder() {
-    }
+	private UseSetWithOrder() {
+	}
 
-    /**
-     * @param args
-     *            ignored
-     */
-    public static void main(final String[] args) {
-        /*
-         * Write a program which:
-         * 
-         * 1) Creates a new ORDERED TreeSet of Strings. To order the set, define
-         * a new Comparator in a separate class.
-         * 
-         * 2) Inserts in the set a hundred strings containing randomly generated
-         * numbers (use Math.random())
-         * 
-         * 3) Prints the set, which must be ordered
-         */
-    }
+	/**
+	 * @param args
+	 *            ignored
+	 */
+	public static void main(final String[] args) {
+		
+		/* Creates a new ORDERED TreeSet of Strings. To order the set, define
+		 * a new Comparator in a separate class.
+		 */
+		TreeSet<String> tree = new TreeSet<String>(new OrderedStringDoubleComparator());
+		
+		/* Inserts in the set a hundred strings containing randomly generated
+		 * numbers (use Math.random())
+		 */
+		for(int i = 0; i < 100; i++) {
+			Double rand = Math.random();
+			tree.add(rand.toString());
+		}
+		
+		// Prints the set, which must be ordered
+		for(String str : tree) {
+			System.out.print(str + ", ");
+		}
+	}
 }
