8b75686a77b8a26970d9c66419914e6249257381
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-12-09 22:52:21 +0100

    fix: reviewed ex1.UseSet

--- src/it/unibo/oop/lab05/ex1/UseSet.java
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -3,7 +3,6 @@ package it.unibo.oop.lab05.ex1;
 import java.util.Iterator;
 import java.util.Set;
 import java.util.TreeSet;
-//import java.util.function.Predicate;
 
 /**
  * Example class using {@link Set}.
@@ -23,14 +22,14 @@ public final class UseSet {
 	public static void main(final String[] args) {
 	
 		// 1 - Builds a TreeSet containing Strings
-		final TreeSet<String> tree = new TreeSet<>();
+		final Set<String> tree = new TreeSet<>();
 	
 		/*
 		 * 2 - Populates such Collection with all the Strings ranging from
 		 *   "1" to "20"
 		 */
-		for(int i = 1; i <= 20; i++) {
-			if(UseSet.SET_RANDOMGEN) {
+		for (int i = 1; i <= 20; i++) {
+			if (UseSet.SET_RANDOMGEN) {
 				tree.add(Integer.toString((int) (i * 100 * Math.random())));
 			} else {
 				tree.add(Integer.toString(i));
@@ -45,11 +44,11 @@ public final class UseSet {
 		 * three
 		 */
 		{
-			Iterator<String> iterator = tree.iterator();
+			final Iterator<String> iterator = tree.iterator();
 			
-			while(iterator.hasNext()) {
+			while (iterator.hasNext()) {
 				String chk = iterator.next();
-				if(Integer.parseInt(chk) % 3 == 0) {
+				if (Integer.parseInt(chk) % 3 == 0) {
 					//tree.remove(chk);
 					iterator.remove();
 				}
@@ -60,18 +59,20 @@ public final class UseSet {
 		 * three
 		 */
 		/*
+		{
 			Iterator<String> iterator = tree.iterator();
 			while(iterator.hasNext()) {
 				String chk = iterator.next();
-				
+					
 				int sumValue = 0;
-				for(int index = 0; index < chk.length(); index++) {
+				for (int index = 0; index < chk.length(); index++) {
 					sumValue += Integer.parseInt(Character.toString(chk.charAt(index)));
 				}
 				//System.out.print(sumValue);
 				if(sumValue % 3 == 0) iterator.remove();
 			}
-		}*/
+		}
+		*/
 	
 		// 5 - Prints the content of the Set
 		System.out.println(tree.toString());
@@ -79,20 +80,19 @@ public final class UseSet {
 		// 5 - Prints the content of the Set using a for-each construct
 		Iterator<String> iterator = tree.iterator();
 		System.out.print("[");
-		while(iterator.hasNext()) {
-			String chk = iterator.next();
+		for(final String chk : tree) {
 			System.out.print(chk + ", ");
 		}
 		System.out.println("]");
 	
 		// 6 - Verifies if all the numbers left in the set are even
 		boolean areAllEven = true;
-		while(iterator.hasNext()) {
+		while (iterator.hasNext()) {
 			String chk = iterator.next();
 			areAllEven = areAllEven && (Integer.parseInt(chk) % 2 == 0);
 			if(Integer.parseInt(chk) % 3 == 0);
 		}
-		if(areAllEven) {
+		if (areAllEven) {
 			System.out.println("Element in the set are all even.");
 		} else {
 			System.out.println("Some element in the set are odd!");
