d1018b56239bc5348352f07c79640ca33972e298
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-12-09 23:01:47 +0100

    fix: reviewed ex3.WarehouseImpl

--- src/it/unibo/oop/lab05/ex3/WarehouseImpl.java
+++ src/it/unibo/oop/lab05/ex3/WarehouseImpl.java
@@ -1,43 +1,63 @@
 package it.unibo.oop.lab05.ex3;
 
+import java.util.Collection;
 import java.util.LinkedHashSet;
 import java.util.Set;
 
+/**
+ * Implementation of a warehouse.
+ *
+ */
 public class WarehouseImpl implements Warehouse {
 
-	private final LinkedHashSet<String> list = new LinkedHashSet<>();
-	private final LinkedHashSet<Product> stock = new LinkedHashSet<>();
+	//private final Set<String> list = new LinkedHashSet<>();
+	//private final Set<Product> stock = new LinkedHashSet<>();
+	private final Set<String> list = newSet();
+	private final Set<Product> stock = newSet();
 
 	public WarehouseImpl() { }
 
+	/*
+	 * {@inheritDoc}
+	 */
 	public void addProduct(Product p) {
 		this.stock.add(p);
 		this.list.add(p.getName());
 	}
 
-	@SuppressWarnings("unchecked")
+	//@SuppressWarnings("unchecked")
 	public Set<String> allNames() {
-		return (Set<String>) this.list.clone();
+		//return (Set<String>) this.list.clone();
 		
-		//Set<String> copySet = new LinkedHashSet<>(this.list);
-		//return copySet;
+		Set<String> copySet = newSetFrom(this.list);
+		return copySet;
 	}
 
-	@SuppressWarnings("unchecked")
+	//@SuppressWarnings("unchecked")
 	public Set<Product> allProducts() {
-		return (Set<Product>) this.stock.clone();
+		//return (Set<Product>) this.stock.clone();
+
+		return newSetFrom(this.stock);
 	}
 
-	public boolean containsProduct(Product p) {
+	public boolean containsProduct(final Product p) {
 		return stock.contains(p);
 	}
 
-	public double getQuantity(String name) {
-		for (Product p : this.stock) {
+	public double getQuantity(final String name) {
+		for (final Product p : this.stock) {
 			if (p.getName().equals(name)) {
 				return p.getQuantity();
 			}
 		}
 		return -1;
+	}
+
+	private static <E> Set<E> newSet() {
+		return new LinkedHashSet<>();
+	}
+	
+	private static <E> Set<E> newSetFrom(final Collection<E> orig) {
+		return new LinkedHashSet<>(orig);
 	}
 }
