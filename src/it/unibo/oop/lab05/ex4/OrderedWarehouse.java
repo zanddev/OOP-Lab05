040b7a9350e010c52405bc68827eea9e47d3e011
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-12-09 23:03:34 +0100

    added Javadoc and one unimplemented method on ex4.OrderedWarehouse

--- src/it/unibo/oop/lab05/ex4/OrderedWarehouse.java
+++ src/it/unibo/oop/lab05/ex4/OrderedWarehouse.java
@@ -5,10 +5,16 @@ import java.util.LinkedHashSet;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 
+import it.unibo.oop.lab05.ex3.Product;
 import it.unibo.oop.lab05.ex3.WarehouseImpl;
 import it.unibo.oop.lab05.ex3.Warehouse;
 
+/**
+ * Implementation of a ordered warehouse.
+
+ */
 public class OrderedWarehouse extends WarehouseImpl implements Warehouse {
 
 	public OrderedWarehouse() {
@@ -16,11 +22,22 @@ public class OrderedWarehouse extends WarehouseImpl implements Warehouse {
 	}
 
 	@Override
+	/**
+	 * {@inheritDoc}
+	 */
 	public Set<String> allNames() {
 		Set<String> copySet = super.allNames();
 		List<String> copyList = new LinkedList<>(copySet);
 		Collections.sort(copyList);
 		//return Set.copyOf(copyList);
 		return new LinkedHashSet<String>(copyList);
+	}
+
+	@Override
+	/**
+	 * {@inheritDoc}
+	 */
+	public Set<Product> allProducts() {
+		return new TreeSet<>(super.allProducts());
 	}
 }
