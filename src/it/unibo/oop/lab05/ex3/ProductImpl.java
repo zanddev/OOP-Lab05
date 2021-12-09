c58d7b15f4aa4c74debd2a48cefee6f680a9a699
Davide Zandonella <davide.zandonella@studio.unibo.it>
2021-12-09 23:00:52 +0100

    added Javadoc and three unimplemented methods on ex2.ProductImpl

--- src/it/unibo/oop/lab05/ex3/ProductImpl.java
+++ src/it/unibo/oop/lab05/ex3/ProductImpl.java
@@ -1,20 +1,62 @@
 package it.unibo.oop.lab05.ex3;
 
+/**
+ * Product implementation.
+ * 
+ */
 public class ProductImpl implements Product {
 
 	private final String name;
-	private double quantity;
+	private final double quantity;
 
-	public ProductImpl(String name, double quantity) {
+	/**
+	 * @param n
+	 *            the product name
+	 * @param q
+	 *            the product quantity
+	 */
+	public ProductImpl(final String name, final double quantity) {
+		super();
 		this.name = name;
 		this.quantity = quantity;
 	}
 
+	/*
+	 * {@inheritDoc}
+	 */
 	public String getName() {
 		return this.name;
 	}
 
+	/*
+	 * {@inheritDoc}
+	 */
 	public double getQuantity() {
 		return this.quantity;
+	}
+
+	/*
+	 * {@inheritDoc}
+	 */
+	public int hashCode() {
+		return name.hashCode();
+	}
+
+	/*
+	 * {@inheritDoc}
+	 */
+	public boolean equals(final Object obj) {
+		if (obj instanceof Product) {
+			final var pr = (Product) obj;
+			return name.equals(pr.getName());
+		}
+		return false;
+	}
+
+	/*
+	 * {@inheritDoc}
+	 */
+	public final String toString() {
+		return "ProductImpl [name=" + name + ", quantity=" + quantity + "]";
 	}
 }
